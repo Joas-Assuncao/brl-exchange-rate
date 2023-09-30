531641fae5ce28dc68e5ad0e479331a15b053f04
Joas-Assuncao <joassalvior@gmail.com>
2023-09-30 02:38:38 -0300

    feat: added exchange list

--- src/app/components/exchange-lasts/exchange-lasts.component.ts
+++ src/app/components/exchange-lasts/exchange-lasts.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DailyExchangeRate } from 'src/app/models/Exchange.models';
+import { calculateDiff } from 'src/app/utils/calculateDiff';
 
 @Component({
   selector: 'app-exchange-lasts',
   templateUrl: './exchange-lasts.component.html',
-  styleUrls: ['./exchange-lasts.component.scss']
+  styleUrls: ['./exchange-lasts.component.scss'],
 })
 export class ExchangeLastsComponent {
+  @Input() exchangeList: DailyExchangeRate[] | undefined;
+  @Input() currentValue: number;
 
+  public calculateDiff = calculateDiff;
+
+  constructor() {
+    this.currentValue = 0;
+  }
 }
