60526b81da32863c437428b3ab7fd6bfac71b827
Joas-Assuncao <joassalvior@gmail.com>
2023-09-29 21:39:20 -0300

    feat: create exchange service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ExchangeService } from './exchange.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  formGroup: FormGroup;
+
+  constructor(
+    private exchangeService: ExchangeService,
+    private formBuilder: FormBuilder
+  ) {
+    this.formGroup = this.formBuilder.group({
+      inputSearch: [
+        '',
+        Validators.required,
+        Validators.maxLength(3),
+        Validators.minLength(3),
+      ],
+    });
+  }
+
+  handleSearch() {
+    this.exchangeService;
+  }
+}
