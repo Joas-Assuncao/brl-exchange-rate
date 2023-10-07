c7ae732f3b1243abdd40665ff66e30f5ebb1aaa7
Joas-Assuncao <joassalvior@gmail.com>
2023-10-06 22:43:07 -0300

    imporve: add one method for endpoint

--- src/app/services/exchange.service.ts
+++ src/app/services/exchange.service.ts
@@ -1,8 +1,12 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
-import { ExchangeRate } from '../models/Exchange.models';
+import {
+  ExchangeRateData,
+  ExchangeRateDaily,
+  ExchangeRateNow,
+} from '../models/Exchange.models';
 
 @Injectable({
   providedIn: 'root',
@@ -15,22 +19,55 @@ export class ExchangeService {
     this.baseURL = 'https://api-brl-exchange.actionlabs.com.br/api/1.0/open';
   }
 
-  listExchange({
+  setHeaders({
     from_symbol,
     to_symbol,
-    path,
   }: {
     from_symbol: string;
     to_symbol: string;
-    path: string;
-  }): Observable<ExchangeRate> {
-    const params: HttpParams = new HttpParams()
+  }): HttpParams {
+    return new HttpParams()
       .append('apiKey', 'RVZG0GHEV2KORLNA')
       .append('from_symbol', from_symbol)
       .append('to_symbol', to_symbol);
+  }
 
-    return this.http.get<ExchangeRate>(`${this.baseURL}${path}`, {
-      params: params,
+  listExchangeNow({
+    from_symbol,
+    to_symbol,
+  }: {
+    from_symbol: string;
+    to_symbol: string;
+  }): Observable<ExchangeRateNow> {
+    const params = this.setHeaders({
+      from_symbol,
+      to_symbol,
     });
+
+    return this.http.get<ExchangeRateNow>(
+      `${this.baseURL}/currentExchangeRate`,
+      {
+        params: params,
+      }
+    );
+  }
+
+  listExchangeLast30Days({
+    from_symbol,
+    to_symbol,
+  }: {
+    from_symbol: string;
+    to_symbol: string;
+  }): Observable<ExchangeRateDaily[]> {
+    const params = this.setHeaders({
+      from_symbol,
+      to_symbol,
+    });
+
+    return this.http
+      .get<ExchangeRateData>(`${this.baseURL}/dailyExchangeRate`, {
+        params: params,
+      })
+      .pipe(map((result) => result.data));
   }
 }
